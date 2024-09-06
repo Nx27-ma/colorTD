621a696662da7c8940a93f83cd8b1bcca506d067
Nxbi <37214@ma-web.nl>
2024-09-07 01:13:20 +0200

    Made MapGen class

--- Assets/Scripts/MapGen.cs
+++ Assets/Scripts/MapGen.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class MapGen : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    GameObject cursorOverlay;
+    List<GameObject> path;
+
+
     void Start()
     {
-        
+        Cursor.SetCursor(Texture2D.redTexture, new Vector2(1,1), CursorMode.Auto);
+        path = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            int i = 0;
+            GameObject drawSphere = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            drawSphere.transform.position = Camera.main.ScreenToWorldPoint(cursorOverlay.transform.position);
+            
+        }
     }
 }
