450865b4651a78515a7721447b1601a1308a9bd4
Nxbi <37214@ma-web.nl>
2024-09-09 22:35:30 +0200

    add more to towerplacement

--- Assets/Scripts/TowerPlacement.cs
+++ Assets/Scripts/TowerPlacement.cs
@@ -1,16 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TowerPlacement : MonoBehaviour
 {
     Vector3 cursorPos;
+    GameObject dragableObj;
+    GameObject canvas;
+    GameObject[] buttonS;
+
     void Start()
     {
-        GameObject.CreatePrimitive(PrimitiveType.Cube);
+        dragableObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        canvas = GameObject.Find("Canvas");
+        
+        Transform frame = canvas.transform.Find("Frame");
+
+        buttonS = new GameObject[frame.childCount];
+
+        for (int i = 0; i < frame.childCount; i++)
+        {
+            buttonS[i] = frame.GetChild(i).gameObject;
+        }
+        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         cursorPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -3));
