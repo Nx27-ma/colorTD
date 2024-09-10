1ad20ecd4b8ae945eec7986140302659f97f7746
Nora_de_Greeuw <146176774+Nx27-ma@users.noreply.github.com>
2024-09-10 17:10:15 +0200

    child getters made

--- Assets/Scripts/UIClickHandler.cs
+++ Assets/Scripts/UIClickHandler.cs
@@ -1,30 +1,62 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIClickHandler : MonoBehaviour
 {
+    //Set all panels on inactive except main
+
     GameObject canvas;
     GameObject[] panels;
     void Start()
     {
         canvas = GameObject.FindGameObjectWithTag("Canvas");
         int childAmount = canvas.transform.childCount;
-        panels = initChildren(childAmount, canvas);
+        panels = getDirectChildren(childAmount, canvas);
+        Type buttonType = typeof(Button);
+        getAllChildrenOfType(panels[(int)PanelType.main], buttonType);
     }
 
-    GameObject[] initChildren(int childAmount, GameObject currentObj)
+    GameObject[] getDirectChildren(int childAmount, GameObject parentObj)
     {
         GameObject[] children = new GameObject[childAmount];
+     
+            for (int i = 0; i < childAmount; i++)
+            {
+                children[i] = parentObj.transform.GetChild(i).gameObject;
+            }   
 
-        for (int i = 0; i < childAmount; i++)
+        return children;
+    }
+
+    GameObject[] getChildrenByName(string[] names, GameObject parentObj)
+    {
+        GameObject[] children = new GameObject[names.Length];
+        for (int i = 0; i < names.Length; i++)
         {
-            children[i] = currentObj.transform.GetChild(i).gameObject;
+            children[i] = parentObj.transform.Find(names[i]).gameObject;
+            if (names[i] != null)
+            {
+                Debug.Log("Warning no name found at index:" + i);
+            }
         }
 
         return children;
+    }
+
+
+    GameObject[] getAllChildrenOfType(GameObject parentObj, Type type)
+    {
+        GameObject[] filtered = parentObj.transform.GetComponentsInChildren(type)
+                  .Select(i => i.gameObject)
+                  //.Take(childAmount)
+                  .ToArray();
+
+        return filtered;
     }
 
     // Update is called once per frame
