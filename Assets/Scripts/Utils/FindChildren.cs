784cc70d08f47f8ca350d5195b8750bcc0877b96
Nxbi <37214@ma-web.nl>
2024-10-13 10:26:29 +0200

    reset FindChildren to previous state

--- Assets/Scripts/Utils/FindChildren.cs
+++ Assets/Scripts/Utils/FindChildren.cs
@@ -10,24 +10,46 @@ namespace Utils
     {
         public static GameObject[] GetDirectChildren(GameObject parentObj, int amount = 0)
         {
-            return parentObj.GetComponentsInChildren<GameObject>().Take(amount).ToArray();
-        }
+            if (amount == 0) amount = parentObj.transform.childCount;
+            GameObject[] children = new GameObject[amount];
 
+
+            for (int i = 0; i < children.Length; i++)
+            {
+                children[i] = parentObj.transform.GetChild(i).gameObject;
+            }
+
+            return children;
+
+            //if (amount == 0) amount = parentObj.transform.childCount;
+            //List<Transform> tempList = parentObj.transform.GetComponentsInChildren<Transform>().Take(amount).ToList(); ;
+            //tempList.RemoveAt(0);
+            //return tempList.Select(i => i.gameObject).ToArray();
+        }
+        /// <summary>
+        /// Returns all the children with the specified names 
+        /// Excludes the parent
+        /// </summary>
+        /// <param name="names"></param>
+        /// <param name="parentObj"></param>
+        /// <returns>GameObject[]</returns>
         public static GameObject[] GetChildrenByName(string[] names, GameObject parentObj)
         {
-            // GameObject[] children = new GameObject[names.Length];
+            GameObject[] children = new GameObject[names.Length];
 
-            return parentObj.GetComponentsInChildren<GameObject>().Where(i => names.ToList().Contains(i.name)).ToArray();
-            //for (int i = 0; i < names.Length; i++)
-            //{
-            //    children[i] = parentObj.transform.Find(names[i]).gameObject;
-            //    if (names[i] != null)
-            //    {
-            //        Debug.Log("Warning no name found at index:" + i);
-            //    }
-            //}
 
-            //return children;
+            for (int i = 0; i < names.Length; i++)
+            {
+                children[i] = parentObj.transform.Find(names[i]).gameObject;
+                if (names[i] != null)
+                {
+                    Debug.Log("Warning no name found at index:" + i);
+                }
+            }
+
+            return children;
+
+            //return parentObj.GetComponentsInChildren<Transform>().Where(i => names.ToList().Contains(i.name)).Select(i => i.gameObject).ToArray();
         }
 
         /// <param name="parentObj">Give the parent of the children you want - Not needed to be direct children</param>
