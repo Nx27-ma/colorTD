22c94e782167f4e87845065ec52d0fe673878873
Nxbi <37214@ma-web.nl>
2024-12-08 19:32:47 +0100

    made the path generation -Offset fix needed

--- Assets/Scripts/Map/PathGeneration.cs
+++ Assets/Scripts/Map/PathGeneration.cs
@@ -5,73 +5,42 @@ using UnityEngine;
 using UnityEngine.UI;
 using Utils;
 
-//NOT USED
+[RequireComponent(typeof(LineRenderer), typeof(MeshCollider))]
 
-//public class PathGeneration : MonoBehaviour
+public class PathGeneration : MonoBehaviour
+{
+    Mesh Mesh;
+    MeshCollider MeshCollider;
+    LineRenderer LineRenderer;
+    float DrawCooldown = 0.01f, DrawCooldownTimer;
+    void Start()
+    {
+        LineRenderer = GetComponent<LineRenderer>();
+        LineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        LineRenderer.positionCount = 0;
+        MeshCollider = GetComponent<MeshCollider>();
+        Mesh = new();
+    }
 
-//{
+    void Update()
+    {
+        DrawCooldownTimer -= Time.deltaTime;
+        if (Input.GetMouseButton(0) && DrawCooldownTimer <= 0)
+        {
+            DrawCooldownTimer = DrawCooldown;
+            LineRenderer.positionCount++;
+            Vector3 CusrorWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-//    GameObject[] pathPoints;
-//    float width = 0.5f;
+            CusrorWorldPos.z = 0;
+            LineRenderer.SetPosition(LineRenderer.positionCount -1, CusrorWorldPos);
 
-
-//    void Start()
-//    {
-//        GameObject path = GameObject.Find("PathPoints");
-//        meshObj = new GameObject();
-//        pathPoints = FindChildren.GetDirectChildren(path);
-
-//        pointA = pathPoints[0].transform.position;
-//        pointB = pathPoints[1].transform.position;
-//        meshFilter = meshObj.AddComponent<MeshFilter>();
-//        MeshRenderer = meshObj.AddComponent<MeshRenderer>();
-
-//        GenerateMesh();
-
-//    }
-
-//    void GenerateMesh()
-//    {
-
-//        Vector3 direction = (pointB - pointA).normalized;
-//        float length = Vector3.Distance(pointA, pointB);
-
-
-//        Vector3 right = Vector3.Cross(direction, Vector3.up).normalized * width / 2;
-
-//        // Vertices of the quad (clockwise)
-//        Vector3[] vertices = new Vector3[4];
-//        vertices[0] = pointA + right;
-//        vertices[1] = pointA - right;
-//        vertices[2] = pointB + right;
-//        vertices[3] = pointB - right;
-
-//        // Create a new mesh
-//        mesh = new Mesh();
-//        mesh.vertices = vertices;
-
-//        // Define the triangles (two triangles for the quad)
-//        int[] triangles = new int[6];
-//        triangles[0] = 0;
-//        triangles[1] = 2;
-//        triangles[2] = 1;
-//        triangles[3] = 2;
-//        triangles[4] = 3;
-//        triangles[5] = 1;
-
-//        // Assign the triangles to the mesh
-//        mesh.triangles = triangles;
-
-//        // Calculate normals for lighting
-//        mesh.RecalculateNormals();
-
-//        // Assign the mesh to the MeshFilter
-//        meshFilter.mesh = mesh;
-//       meshObj.transform.localEulerAngles = new Vector3(-90, 0, 0);
-
-//    }
+            LineRenderer.BakeMesh(Mesh, true);
+            MeshCollider.sharedMesh = Mesh;
+        }
+    }
+}
 
    
-//}
+
 
 
