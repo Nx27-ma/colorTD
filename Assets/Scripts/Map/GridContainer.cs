acdb7c126a1c044526c4081007f2e5dcbf849dac
Nora_de_Greeuw <146176774+Nx27-ma@users.noreply.github.com>
2025-02-02 22:44:11 +0100

    started with gridcontainer class and made ICellContent

--- Assets/Scripts/Map/GridContainer.cs
+++ Assets/Scripts/Map/GridContainer.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class GridContainer
 {
-  public GridContainer()
+  ICellContent[,] Grid;
+  int x, y;
+  public GridContainer(int x, int y)
   {
-    
+    this.x = x;
+    this.y = y;
+    Grid = generateMapArrays<ICellContent>(x, y);
+    ICellContent.GetAllOfType<ICellContent>();
+  }
+
+  T[,] generateMapArrays<T>(int x, int y) 
+  {
+    T[,] ints = new T[x,y];
+    return ints;
   }
 }
